f3ed2f9f20de6624eed63b657e231eb9f23720e7
GianDigia <digiacomogianmarco@gmail.com>
2019-10-12 17:02:03 +0200

    Add well-look controller

--- static/controller.js
+++ static/controller.js
@@ -1,5 +1,29 @@
-<h1>ciao</h1>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js" integrity="sha256-OgiHfllOBEOiWQ2OcBsDncRG2k7QBW9Rs1dB7MH2N2s=" crossorigin="anonymous"></script>
-<script>
-  var socket = io();
-</script>
+let direction = 0
+var socket = io("/controllers")
+
+function setleft() {
+  direction = -1
+  send()
+}
+function setright() {
+  direction = 1
+  send()
+}
+function setnull() {
+  direction = 0
+  send()
+}
+
+function send() {
+  socket.emit("direction", direction)
+}
+
+document.querySelector("#left").addEventListener("mousedown", setleft)
+document.querySelector("#left").addEventListener("mouseup", setnull)
+document.querySelector("#right").addEventListener("mousedown", setright)
+document.querySelector("#right").addEventListener("mouseup", setnull)
+
+document.querySelector("#left").addEventListener("touchstart", setleft)
+document.querySelector("#left").addEventListener("touchend", setnull)
+document.querySelector("#right").addEventListener("touchstart", setright)
+document.querySelector("#right").addEventListener("touchend", setnull)
